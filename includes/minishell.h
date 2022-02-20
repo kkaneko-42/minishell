20ce26fd0a6b9dfe2a5317a0825c612a44cda3b6
Kazuhiko Kaneko <kkaneko@c1r20s10.42tokyo.jp>
2022-02-20 12:51:45 +0900

    create includes/minishell.h

---
+++ includes/minishell.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkaneko </var/mail/kkaneko>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/20 12:50:37 by kkaneko           #+#    #+#             */
+/*   Updated: 2022/02/20 12:51:30 by kkaneko          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#endif
