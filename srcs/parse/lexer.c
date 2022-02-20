e9d9b0de6deb1f5b89295493f8288677441a9cb1
Kazuhiko Kaneko <kkaneko@c1r20s10.42tokyo.jp>
2022-02-20 15:31:50 +0900

    lexer.c modified

--- srcs/parse/lexer.c
+++ srcs/parse/lexer.c
@@ -6,14 +6,27 @@
 /*   By: kkaneko </var/mail/kkaneko>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/20 14:21:59 by kkaneko           #+#    #+#             */
-/*   Updated: 2022/02/20 14:30:37 by kkaneko          ###   ########.fr       */
+/*   Updated: 2022/02/20 15:27:32 by kkaneko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+static char	**split_cmd_arg(const char *input);
+
 char	**lexer(const char *input)
 {
 	if (input == NULL)
 		return (NULL);
+}
+
+static char	**split_cmd_arg(const char *input)
+{
+	size_t	i;
+
+	i = 0;
+	skip_whitespace(input, &i);
+	while (input[i] != 0x00)
+	{
+	}
 }
