5fb7b533c68d885d74091039388f11a81efba7b5
kkaneko <kkaneko@student.42tokyo.jp>
2022-03-05 18:00:25 +0900

    save

--- srcs/expand_env.c
+++ srcs/expand_env.c
@@ -6,13 +6,13 @@
 /*   By: kkaneko <kkaneko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 13:19:05 by kkaneko           #+#    #+#             */
-/*   Updated: 2022/03/05 15:34:45 by kkaneko          ###   ########.fr       */
+/*   Updated: 2022/03/05 17:59:19 by kkaneko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	try_expand(char *str);
+static void	do_expand(char *str);
 static char	*get_env_name_from_dollar(char *str);
 
 void	expand_env(t_list *token)
@@ -23,29 +23,27 @@ void	expand_env(t_list *token)
 	while (now_token != NULL)
 	{
 		if (now_token->content[0] != '\'')
-			try_expand(now_token->content);
+			do_expand(now_token->content);
 		now_token = now_token->next;
 	}
 }
 
-static void	try_expand(char *str)
+static void	do_expand(char *str)
 {
 	size_t			i;
-	size_t			env_start_i;
-	size_t			env_end_i;
-	char			*env_name;
+	char			*env_value;
 
 	i = 0;
 	while (str[i] != 0x00)
 	{
 		if (str[i] == '$')
-			env_name = get_env_name_from_dollar(&str[i]);
+			env_value = get_env_value_from_dollar(&str[i]);
 		++i;
 	}
 }
 
 //str[0] is $
-static char	*get_env_name_from_dollar(char *str)
+static char	*get_env_value_from_dollar(char *str)
 {
 	char	*env_name;
 	size_t	env_start_i;
