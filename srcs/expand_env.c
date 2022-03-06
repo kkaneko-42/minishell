5b9ea682af47c0aa3c797d674bd1f2a182db4ad8
kkaneko <kkaneko@student.42tokyo.jp>
2022-03-06 18:15:27 +0900

    save

--- srcs/expand_env.c
+++ srcs/expand_env.c
@@ -6,16 +6,16 @@
 /*   By: kkaneko <kkaneko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 13:19:05 by kkaneko           #+#    #+#             */
-/*   Updated: 2022/03/05 17:59:19 by kkaneko          ###   ########.fr       */
+/*   Updated: 2022/03/06 17:54:23 by kkaneko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "../includes/minishell.h"
 
-static void	do_expand(char *str);
-static char	*get_env_name_from_dollar(char *str);
+static void	try_expand(char *str, t_envp *env_list);
+static char	*get_env_name(char *str);
 
-void	expand_env(t_list *token)
+void	expand_env(t_list *token, t_envp *env_list)
 {
 	t_list	*now_token;
 
@@ -23,34 +23,23 @@ void	expand_env(t_list *token)
 	while (now_token != NULL)
 	{
 		if (now_token->content[0] != '\'')
-			do_expand(now_token->content);
+			try_expand(now_token->content, env_list);
 		now_token = now_token->next;
 	}
 }
 
-static void	do_expand(char *str)
+static void	try_expand(char *str)
 {
 	size_t			i;
-	char			*env_value;
+	char			*env_name;
 
 	i = 0;
 	while (str[i] != 0x00)
 	{
 		if (str[i] == '$')
-			env_value = get_env_value_from_dollar(&str[i]);
+		{
+			env_name = get_env_name(&str[i]);
+		}
 		++i;
 	}
-}
-
-//str[0] is $
-static char	*get_env_value_from_dollar(char *str)
-{
-	char	*env_name;
-	size_t	env_start_i;
-	size_t	env_len;
-
-	env_start_i = 1;
-	env_len = 0;
-	//may need to support other space(tab for example)
-	while (str[env_len + 1] != ' ')
 }
