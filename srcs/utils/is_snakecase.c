7b4101e758af85344554b8a4eebb0f7e45115bc6
Hyu-waseda <95074991+Hyu-waseda@users.noreply.github.com>
2022-03-07 00:48:48 +0900

    Update is_snakecase.c

--- srcs/utils/is_snakecase.c
+++ srcs/utils/is_snakecase.c
@@ -16,7 +16,7 @@ int	is_snakecase(char c, size_t i)
 {
 	if (i == 0 && ft_isdigit(c))
 		return (0);
-	else if (ft_isalnum(c))
+	else if (ft_isalnum(c) || c == '_')
 		return (1);
 	else
 		return (0);
