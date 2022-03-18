0a413a6323e9e5ae6d718f95fcb7450699080f0e
kkaneko <kkaneko@student.42tokyo.jp>
2022-03-18 23:48:50 +0900

    fix error: quotation in quote case

--- srcs/parse/validate/check_nb_quotes.c
+++ srcs/parse/validate/check_nb_quotes.c
@@ -6,14 +6,15 @@
 /*   By: kkaneko <kkaneko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/13 21:07:43 by kkaneko           #+#    #+#             */
-/*   Updated: 2022/03/16 00:59:07 by kkaneko          ###   ########.fr       */
+/*   Updated: 2022/03/18 23:47:41 by kkaneko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static size_t	count_quotes(const char *str, const char quote_type);
-static int		is_odd(size_t n);
+static void	count_quotes(const char *str, size_t *nb_double, size_t *nb_single);
+static int	quotation_is_quoted(size_t nb_another_quotes);
+static int	is_odd(size_t n);
 
 int	check_nb_quotes(const t_list *tokens)
 {
@@ -26,8 +27,7 @@ int	check_nb_quotes(const t_list *tokens)
 	now = (t_list *)tokens;
 	while (now != NULL)
 	{
-		nb_double_quotes = count_quotes(now->content, '\"');
-		nb_single_quotes = count_quotes(now->content, '\'');
+		count_quotes(now->content, &nb_double_quotes, &nb_single_quotes);
 		if (is_odd(nb_double_quotes) || is_odd(nb_single_quotes))
 			return (1);
 		now = now->next;
@@ -35,20 +35,29 @@ int	check_nb_quotes(const t_list *tokens)
 	return (0);
 }
 
-static size_t	count_quotes(const char *str, const char quote_type)
+static void	count_quotes(const char *str, size_t *nb_double, size_t *nb_single)
 {
 	size_t	i;
-	size_t	res;
 
-	res = 0;
 	i = 0;
 	while (str[i] != 0x00)
 	{
-		if (str[i] == quote_type)
-			++res;
+		if (str[i] == '\"' && !quotation_is_quoted(*nb_single))
+			++(*nb_double);
+		else if (str[i] == '\'' && !quotation_is_quoted(*nb_double))
+			++(*nb_single);
 		++i;
 	}
-	return (res);
+}
+
+//Whether the quotation is surrounded by another one
+//(eg. Is double quote(") surrounded by single quote(')?)
+static int	quotation_is_quoted(size_t nb_another_quotes)
+{
+	if (is_odd(nb_another_quotes))
+		return (1);
+	else
+		return (0);
 }
 
 static int	is_odd(size_t n)
