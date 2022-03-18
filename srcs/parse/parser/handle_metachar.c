9c8f00a8096395a3ff30626ed98d9b9a597e34f1
kkaneko <kkaneko@student.42tokyo.jp>
2022-03-19 00:24:08 +0900

    fd close in case of multiple redirections

--- srcs/parse/parser/handle_metachar.c
+++ srcs/parse/parser/handle_metachar.c
@@ -6,14 +6,15 @@
 /*   By: kkaneko <kkaneko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 17:06:13 by kkaneko           #+#    #+#             */
-/*   Updated: 2022/03/18 16:36:28 by kkaneko          ###   ########.fr       */
+/*   Updated: 2022/03/19 00:10:22 by kkaneko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static char	*get_heredoc_input(const char *end_text);
-static char	*get_file_content_all(int fd);
+char	*get_heredoc_input(const char *end_text);
+char	*get_file_content_all(int fd);
+int		open_output_file(const char *file_name, int fg_append);
 
 void	input_file_specify(t_cmd *cmd, t_list **token)
 {
@@ -36,29 +37,9 @@ void	heredoc(t_cmd *cmd, t_list **token)
 
 	*token = (*token)->next;
 	remove_quotes(&((*token)->content));
-	free(cmd->stdin_str);
 	end_text = (*token)->content;
+	free(cmd->stdin_str);
 	cmd->stdin_str = get_heredoc_input(end_text);
-}
-
-static char	*get_heredoc_input(const char *end_text)
-{
-	char	*line;
-	char	*res;
-
-	res = NULL;
-	line = readline(HEREDOC_PROMPT);
-	while (line != NULL
-		&& ft_strncmp(line, end_text, ft_strlen(end_text) + 1) != 0)
-	{
-		res = ft_stradd(&res, line);
-		res = ft_stradd(&res, "\n");
-		free(line);
-		line = readline(HEREDOC_PROMPT);
-	}
-	//eof validation
-	free(line);
-	return (res);
 }
 
 void	output_file_specify(t_cmd *cmd, t_list **token, int fg_append)
@@ -66,34 +47,11 @@ void	output_file_specify(t_cmd *cmd, t_list **token, int fg_append)
 	const mode_t	out_file_rights = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
 	const int		open_flags = O_RDWR | O_CREAT | fg_append;
 	char			*file_name;
-	int				fd_out;
 
 	*token = (*token)->next;
 	remove_quotes(&((*token)->content));
 	file_name = (*token)->content;
-	if (fg_append != O_APPEND)
-	{
-		if (unlink(file_name) && errno != ENOENT)
-			perror(file_name);
-	}
-	fd_out = open(file_name, open_flags, out_file_rights);
-	if (fd_out == -1)
-		perror(file_name);
-	cmd->fd_out = fd_out;
-}
-
-static char	*get_file_content_all(int fd)
-{
-	char	*res;
-	char	*tmp;
-
-	res = NULL;
-	tmp = get_next_line(fd);
-	while (tmp != NULL) //EOF?error?
-	{
-		res = ft_stradd(&res, tmp);
-		free(tmp);
-		tmp = get_next_line(fd);
-	}
-	return (res);
+	if (cmd->fd_out > STDERR_FILENO)
+		close(cmd->fd_out);
+	cmd->fd_out = open_output_file(file_name, fg_append);
 }
