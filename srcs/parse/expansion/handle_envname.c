85bc0163092ac8353b19d6a948487317fe96c86f
kkaneko <kkaneko@student.42tokyo.jp>
2022-03-19 23:46:14 +0900

    support $?

--- srcs/parse/expansion/handle_envname.c
+++ srcs/parse/expansion/handle_envname.c
@@ -6,7 +6,7 @@
 /*   By: kkaneko <kkaneko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 01:32:16 by kkaneko           #+#    #+#             */
-/*   Updated: 2022/03/18 01:33:48 by kkaneko          ###   ########.fr       */
+/*   Updated: 2022/03/19 23:44:55 by kkaneko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,16 @@ size_t	get_envname_head_index(char *str)
 
 size_t	get_envname_tail_index(char *str)
 {
-	size_t	i;
-	size_t	j;
+	size_t	start_i;
+	size_t	now_i;
 
-	i = get_envname_head_index(str);
-	j = 0;
-	while (is_snakecase(str[i + j], j))
-		++j;
-	return (i + j - 1);
+	start_i = get_envname_head_index(str);
+	now_i = start_i;
+	if (str[now_i] == '?')
+		return (now_i);
+	while (is_snakecase(str[now_i], now_i - start_i))
+		++now_i;
+	return (now_i - 1);
 }
 
 char	*get_env_name_from_token(char *str)
