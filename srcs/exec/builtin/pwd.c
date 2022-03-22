1eda5f47d34d210dc0bfd9c154bb905f72dd260e
okumura hyu <okumura.hyu@akane.waseda.jp>
2022-03-22 13:41:35 +0900

    upgrade cd set /Users/okumurahyu/42tokyo/minishell/minishell and /Users/okumurahyu/42tokyo/minishell

--- srcs/exec/builtin/pwd.c
+++ srcs/exec/builtin/pwd.c
@@ -6,13 +6,11 @@
 /*   By: okumurahyu <okumurahyu@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 17:06:30 by okumurahyu        #+#    #+#             */
-/*   Updated: 2022/03/21 00:36:10 by okumurahyu       ###   ########.fr       */
+/*   Updated: 2022/03/22 12:46:28 by okumurahyu       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/minishell.h"
-
-static int	is_empty(char *s);
 
 void	pwd(t_cmd *input)
 {
@@ -23,18 +21,10 @@ void	pwd(t_cmd *input)
 	{
 		ft_putstr_fd("minishell: pwd: ", STDERR_FILENO);
 		ft_putendl_fd(strerror(errno), STDERR_FILENO);
-		free(now_path);
 		errno = 0;
 		return ;
 	}
 	ft_putstr_fd(now_path, input->fd_out);
 	ft_putstr_fd("\n", input->fd_out);
 	free(now_path);
-}
-
-static int	is_empty(char *s)
-{
-	if (s[0] == '\0')
-		return (1);
-	return (0);
 }
