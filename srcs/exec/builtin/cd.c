225011d172524605a9ef0076a325e26681d015d0
okumura hyu <okumura.hyu@akane.waseda.jp>
2022-03-30 00:05:04 +0900

    update cd -

--- srcs/exec/builtin/cd.c
+++ srcs/exec/builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: okumurahyu <okumurahyu@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 08:39:05 by okumurahyu        #+#    #+#             */
-/*   Updated: 2022/03/27 11:37:12 by okumurahyu       ###   ########.fr       */
+/*   Updated: 2022/03/29 23:59:34 by okumurahyu       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,12 @@ int	cd(t_cmd *input, t_envp *envp)
 	old_path = get_now_path();
 	new_path = get_new_path(input, envp);
 	errno = 0;
-	if (chdir(new_path))
+	if (new_path == NULL)
+	{
+		free(old_path);
+		return (no_old_pwd_err());
+	}
+	else if (chdir(new_path))
 	{
 		free(old_path);
 		free(new_path);
@@ -51,6 +56,14 @@ static char	*get_new_path(t_cmd *input, t_envp *envp)
 					getenv("HOME"), "/", &(input->args->content)[2]));
 		return (three_strjoin(
 				ft_getenv("HOME", envp), "/", &(input->args->content)[2]));
+	}
+	else if (ft_strncmp(input->args->content, "-", 2) == 0)
+	{
+		if (ft_getenv("OLDPWD", envp) == NULL
+			|| is_empty_str(ft_getenv("OLDPWD", envp)))
+			return (NULL);
+		else
+			return (ft_strdup(ft_getenv("OLDPWD", envp)));
 	}
 	return (ft_strdup(input->args->content));
 }
